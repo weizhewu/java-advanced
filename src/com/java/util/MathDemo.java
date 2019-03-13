c2bb89cb1564d9a2414f58ed41e0a316618e93ee
weizhewu <weizhewu232623@gmail.com>
2019-03-13 20:31:31 +0800

    third day

--- src/com/java/util/MathDemo.java
+++ src/com/java/util/MathDemo.java
@@ -1,4 +1,14 @@
 package com.java.util;
 
+/**
+ * 数学类Math的程序
+ * @author 位哲武
+ * 2019.3.12
+ */
 public class MathDemo {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(Math.sqrt(num));
+        System.out.printf("%.2f",Math.sqrt(num));
+    }
 }
